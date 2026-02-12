154f1c56a9ef2f313221f2a7abc9c988afff134f
user <yoqubov11nodir@gmail.com>
2026-02-12 15:46:56 +0500

    'final'

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import get_info, create_product
+from .views import get_info, create_product, list_product, detail_product, patch_product, delete_product
 
 urlpatterns = [
     path('get-info/', get_info),
-    path('create/', create_product)
+    path('create/', create_product),
+    path('list/', list_product),
+    path('detail/<int:pk>/', detail_product),
+    path('patch-update/<int:pk>/', patch_product),
+    path('delete/<int:pk>/', delete_product),
 ]
--- products/views.py
+++ products/views.py
@@ -2,24 +2,87 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 
 from .serializers import ProductSerializer
+from .models import Product
+
 
 @api_view(['GET', ])
 def get_info(request):
     data = {
         'success': True,
-        'massage': 'Hammasi yaxshi'
+        'message': 'Hammasi yaxshi'
     }
     return Response(data)
 
 @api_view(['POST', ])
 def create_product(request):
-    serializer = ProductSerializer(date = request.data)
+    serializer = ProductSerializer(data = request.data, files=request.FILES)
     if serializer.is_valid():
         serializer.save()
 
-        date = {
-
+        data = {
+            'success':True,
+            'message': 'Maxsulot yaratildi',
+            'date': serializer.data
         }
         return Response(serializer.data)
+    raise ValidationError(serializer.errors)
+
+@api_view(['GET', ])
+def list_product(request):
+    products = Product.objects.all()[::-1]
+    if len(products) == 0:
+        raise ValueError('Malumot yoq')
+    serializer = ProductSerializer(products, many=True)
+
+    date = {
+        'success':True,
+        'massage': 'Product',
+        'date': serializer.data
+    }
+    return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def detail_product(request, pk):
+    product = Product.objects.get(pk=pk).first()
+    if product is None:
+        raise ValueError('Malumot topilmadi')
+    serializer = ProductSerializer(product)
+
+    date = {
+        'success':True,
+        'massage': 'Product',
+        'date': serializer.data
+    }
+    return Response(serializer.data)
+
+
+@api_view(['PATCH', ])
+def patch_product(request, pk):
+    product = Product.objects.get(pk=pk).first()
+    serializer = ProductSerializer(product, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        data = {
+            'success':True,
+            'message': 'Maxsulot yaratildi',
+            'date': serializer.data
+        }
+        return Response(serializer.data)
+    raise ValidationError(serializer.errors)
+
+@api_view(['DELETE', ])
+def delete_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    product.delete() 
+
+    data = {
+        'success': True,
+        'message': 'Maxsulot ochchirildi'
+    }
+    return Response(data)
